d20afc7da204d11a8bf82b391a3327d27565ad78
Riquelme Montijo <riquelmesouzam@gmail.com>
2025-03-02 13:57:27 -0300

    Initial commit

---
+++ src/main/java/com/jfore/JforeApplication.java
@@ -0,0 +1,13 @@
+package com.jfore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JforeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JforeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jfore/JforeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JforeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
